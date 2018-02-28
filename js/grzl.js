77e560375847101847d5518eb797ebe88657d9f6
my <470474080@qq.com>
2018-02-28 19:35:53 +0800

    改样式~~~

--- js/grzl.js
+++ js/grzl.js
@@ -1,13 +1,13 @@
 $(function () {
-    $("#box").on("tap",function(){
-        console.log($(this));
+    var resultFl=null;
+    $("#box").on("click",function(e){
         $("#file").click();
     });
     $("#file").on("change",function(){
-        var resultFl=null;
         var fl = $(this).get(0).files[0];
         resultFl=fl;
-        var fileSize=fl/1024;
+        var fileSize=fl.size/1024;
+        console.log(fileSize);
         if(fileSize>100){
             alert("图片大小不能超过100k");
             return false;
@@ -25,70 +25,66 @@ $(function () {
         imgs.onload=function () {
             window.URL.revokeObjectURL(fl);
         };
-        $("#btn").on("tap",function(){
-            var data = new FormData();
-            data.append("name",$("input[name='uname']").val());
-            data.append("photo",$("input[name='photo']").val());
-            data.append("title",$("input[name='title']").val());
-            data.append("img",resultFl);
-            console.log(data);
-            if($("#uname").val()==""||$("#tel").val()==""||$("#tie").val()==""){
+    });
+    $("#btn").on("click",function(){
+        var data = new FormData();
+        data.append("name",$("input[name='username']").val());
+        data.append("tel",$("input[name='tel']").val());
+        data.append("title",$("input[name='title']").val());
+        data.append("photo",resultFl);
+        if($("#uname").val()==""||$("#tel").val()==""||$("#tie").val()==""){
+            $(".dding").css("display","block");
+            $(".dding p").eq(0).html("未修改");
+            $(".ti").on("tap",function(){
+                $(".dding").css("display","none");
+            });
+            return fa;
+        }
+        $.ajax({
+            type:"post",
+            url:"php/server.php",
+            data:data,
+            dataType:"json",
+            processData:false,
+            contentType:false,
+            success:function(da){
+                var img = $("<img src='"+da.photo.replace('../',"")+"' id='img' class='top'/>");
+                $("#box").html(img);
+                localStorage.setItem("img",da.photo);
+                localStorage.setItem("name",$("#uname").val());
                 $(".dding").css("display","block");
-                $(".dding p").eq(0).html("未修改");
-                $(".ti").on("tap",function(){
-                    $ (".dding").css("display","none");
-                    return;
+                $(".dding").html("修改成功");
+                $(".ti").on("click",function(){
+                    $(".dding").css("display","none");
+                    var da=$("form").serialize();
+                    $.ajax({
+                        type:"post",
+                        url:"../php/index.php?c=Message&a=sendMessage",
+                        data:da,
+                        dataType:"json",
+                        success:function(data){
+                            console.log(data);
+                            if(data.code=="403"){
+                                $(".dding").css("display","block");
+                                $(".dding p").eq(0).html(data.message);
+                                $(".ti").on("click",function(){
+                                    $(".dding").css("display","none");
+                                });
+                            }else if(data.code=="200"){
+                                $(".dding").css("display","block").css("top","120%");
+                                $(".dding p").eq(0).html(data.message);
+                                $(".ti").on("click",function(){
+                                    $(".dding").css("display","none");
+                                    location.reload(true);
+                                });
+
+                            }
+                        }
+                    });
+                    window.location.href="index.html";
                 });
             }
-            $.ajax({
-                type:"post",
-                url:"php/server.php",
-                data:data,
-                dataType:"json",
-                processData:false,
-                contentType:false,
-                success:function(da){
-                    var img = $("<img src='"+da.photo.replace('../'," ")+"' id='img' class='top'/>");
-                    box.innerHTML=img;
-                    localStorage.setItem("img",da.photo);
-                    localStorage.setItem("name",$("#uname").val());
-                    $(".dding").css("display","block");
-                    $(".dding").html("修改成功");
-                    $(".ti").on("click",function(){
-                        $(".dding").css("display","none");
-                        var da=$("form").serialize();
-                        $.ajax({
-                            type:"post",
-                            url:"../php/index.php?c=Message&a=sendMessage",
-                            data:da,
-                            dataType:"json",
-                            success:function(data){
-                                if(data.code=="403"){
-                                    $(".dding").css("display","block");
-                                    $(".dding p").eq(0).html(data.message);
-                                    $(".ti").on("click",function(){
-                                        $(".dding").css("display","none");
-                                    });
-                                }else if(data.code=="200"){
-                                    $(".dding").css("display","block").css("top","120%");
-                                    $(".dding p").eq(0).html(data.message);
-                                    $(".ti").on("click",function(){
-                                        $(".dding").css("display","none");
-                                        location.reload(true);
-                                    });
-
-                                }
-                            }
-                        });
-
-
-
-
-                        window.location.href="index.html";
-                    });
-                }
-            })
-
         })
-    });
+
+    })
 });
--- php/server.php
+++ php/server.php
@@ -1,7 +1,8 @@
 <?php
 $name = isset($_POST['name'])? $_POST['name'] : '';
 $gender = isset($_POST['gender'])? $_POST['gender'] : '';
-$target = "../img/";
+$tel = isset($_POST['tel'])? $_POST['gender'] : '';
+$target = "../upload/";
 $filename = $target.$_FILES['photo']['name'];
 
 $response = array();
@@ -14,5 +15,4 @@ if(move_uploaded_file($_FILES['photo']['tmp_name'], $filename)){
 }else{
     $response['isSuccess'] = false;
 }
-
 echo json_encode($response);
