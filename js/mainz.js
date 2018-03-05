1b4ed1eecd292f1d17dce0cc1b68180c72d7f346
15735106685 <>
2018-03-05 19:44:17 +0800

    整合

--- js/mainz.js
+++ js/mainz.js
@@ -1,15 +1,41 @@
 
 $(function(){
-    var tf =true;
-    $(".tc").on("click",function(){
-        if(tf){
-            $(".box_left").css("left","0px");
-            $(".box_right").css("left","4.4rem");
-            tf = false;
-        }else{
-            $(".box_left").css("left","-4.4rem");
-            $(".box_right").css("left","0px");
-            tf = true;
-        }
-    })
+    function Move(){
+        this.tf=true;
+        this.init();
+    }
+    Move.prototype={
+        init:function(){
+            this.auto();
+            this.href();
+        },
+        auto:function(){
+            $(".tc").on("click",function(){
+                if(this.tf){
+                    $(".box_left").css("left","0px");
+                    $(".box_right").css("left","4.4rem");
+                    this.tf = false;
+                }else{
+                    $(".box_left").css("left","-4.4rem");
+                    $(".box_right").css("left","0px");
+                    this.tf = true;
+                }
+            }.bind(this));
+        },
+        //点击个人头像和名字跳转页面
+        href:function(){
+            $("#uname").on("click",function(){
+                window.location.href="grzl.html";
+            });
+            $("#people img").on("click",function(){
+                window.location.href="grzl.html";
+            });
+    }
+
+    }
+    var move=new Move();
+
+
+
+
 });
